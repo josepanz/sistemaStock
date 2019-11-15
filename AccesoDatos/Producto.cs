5e250425a891da2bacc5e6d693acb9998b1fd17e
sebastianpratt <sebastianpratt98@gmail.com>
2019-11-15 11:18:37 -0400

    Creacion de la clase unidad de medida

--- AccesoDatos/Producto.cs
+++ AccesoDatos/Producto.cs
@@ -1,25 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Clases
 {
     public class Producto
     {
+        public int id { get; set; }
         public string descripcion { get; set; }
+        public string codBarra { get; set; }
+        public int precio { get; set; }
+        public int cantidad { get; set; }
         public Marca marca { get; set; }
-        public Categoria categoria { get; set; }
         public TipoProducto tipoProducto { get; set; }
         public Proveedor proveedor { get; set; }
-        public Double precio { get; set; }
+        public 
+        public Categoria categoria { get; set; }
+        
+        
         public DateTime fechaVencimiento { get; set; }
         public static List<Producto> listaProductos = new List<Producto>();
 
         public static void AgregarProductos(Producto P)
         {
-            listaProductos.Add(P);
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+
+            {
+                con.Open();
+                string textoCmd = "INSERT INTO Producto (idNumero, nombre, email, fechaNacimiento, codCargo)VALUES (@idNumero, @nombre, @email, @fc, @cargo)";
+                SqlCommand cmd = new SqlCommand(textoCmd, con);
+                cmd = empl.ObtenerParametros(cmd);
+                cmd.ExecuteNonQuery();
+            }
         }
 
         public static void EliminarProductos(int posicion_item)
---
+++ AccesoDatos/UnidadMedida.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class UnidadMedida
+    {
+        public string codigo { get; set; }
+        public string descripcion { get; set; }
+
+        public static List<UnidadMedida> listaUnidad = new List<UnidadMedida>();
+
+        public static void AgregarUnidad(UnidadMedida UM)
+        {
+            listaUnidad.Add(UM);
+        }
+
+        public static void EliminarUnidad(int posicion_item)
+        {
+            listaUnidad.RemoveAt(posicion_item);
+        }
+
+        public static List<UnidadMedida> ObtenerUnidad()
+        {
+            return listaUnidad;
+        }
+
+        public override string ToString()
+        {
+            return this.descripcion;
+        }
+    }
+}
