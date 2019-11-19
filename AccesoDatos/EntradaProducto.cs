4c8ae3730bdd3135f795da8877284ac151077a43
josepanz <josepanza1@gmail.com>
2019-11-19 00:35:36 -0300

    proceso de Entrada, con su respectiva clase y formulario

--- AccesoDatos/EntradaProducto.cs
+++ AccesoDatos/EntradaProducto.cs
@@ -10,6 +10,138 @@ namespace Clases
 {
     public class EntradaProducto
     {
-       
+        //id, fechaRecepcion, nroDocumento, receptor, direccion
+        public int id { get; set; }
+        public int nrodocumento { get; set; }
+        public string receptor { get; set; }
+        public string direccion { get; set; }
+        public DateTime fecharecepcion { get; set; }
+        public List<DetalleEntradaProducto> detalle = new List<DetalleEntradaProducto>();
+
+        public static List<EntradaProducto> listaEntrada = new List<EntradaProducto>();
+
+        public static void Agregar(EntradaProducto p)
+        {
+            //listaPedidos.Add(p);
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+            {
+                con.Open();
+                //cabecera
+                string textoCMD = "INSERT INTO recepcion (fecharecepcion, nrodocumento, receptor, direccion) output INSERTED.id VALUES (@fecharecepcion, @nrodocumento, @receptor, @direccion)";
+                SqlCommand cmd = new SqlCommand(textoCMD, con);
+                SqlTransaction transaction = con.BeginTransaction("SampleTransaction");
+                cmd.Transaction = transaction;
+                //parametros
+                SqlParameter p1 = new SqlParameter("@fecharecepcion", p.fecharecepcion);
+                SqlParameter p2 = new SqlParameter("@nrodocumento", p.nrodocumento);
+                SqlParameter p3 = new SqlParameter("@receptor", p.receptor);
+                SqlParameter p4 = new SqlParameter("@direccion", p.direccion);
+
+                p1.SqlDbType = System.Data.SqlDbType.DateTime;
+                p2.SqlDbType = System.Data.SqlDbType.Int;
+                p3.SqlDbType = System.Data.SqlDbType.VarChar;
+                p4.SqlDbType = System.Data.SqlDbType.VarChar;
+                cmd.Parameters.Add(p1);
+                cmd.Parameters.Add(p2);
+                cmd.Parameters.Add(p3);
+                cmd.Parameters.Add(p4);
+
+                int id_recepcion = (int)cmd.ExecuteScalar();
+
+
+                //DETALLE
+                foreach (DetalleEntradaProducto dp in p.detalle)
+                {
+
+                    //insert para el detalle
+                    string textoCMD2 = "INSERT INTO DetalleRecepcion(producto_id, cantidadRecibida, recepcion_id) VALUES (@producto_id, @cantidadRecibida, @recepcion_id)";
+                    SqlCommand cmd2 = new SqlCommand(textoCMD2, con);
+                    //Pasamos los parametros
+
+                    SqlParameter p5 = new SqlParameter("@producto_id", dp.producto.id);
+                    SqlParameter p6 = new SqlParameter("@cantidadRecibida", dp.cantidad);
+                    SqlParameter p7 = new SqlParameter("@recepcion_id", id_recepcion);
+                    if (ActualizarStock(dp.producto.id, dp.cantidad) == 1)
+                    {
+                        p5.SqlDbType = System.Data.SqlDbType.Int;
+                        p6.SqlDbType = System.Data.SqlDbType.Int;
+                        p7.SqlDbType = System.Data.SqlDbType.Int;
+                        cmd2.Parameters.Add(p5);
+                        cmd2.Parameters.Add(p6);
+                        cmd2.Parameters.Add(p7);
+                        cmd2.Transaction = transaction;
+                        cmd2.ExecuteNonQuery();
+                        transaction.Commit();
+                    }
+                    else
+                    {
+                        transaction.Rollback();
+                        break;
+                    }
+                }
+                con.Close();
+            }
+        }
+
+        public static int ActualizarStock(int product_id, int cantidad)
+        {
+            int ban = 0;
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+            {
+                con.Open();
+                //cabecera
+                Producto pro = new Producto();
+                pro = Producto.ObtenerProducto(product_id);
+                if (pro != null)
+                {
+                    string textoCMD = "update producto set cantidad = @cantidad  where id = @id";
+                    SqlCommand cmd = new SqlCommand(textoCMD, con);
+                    SqlTransaction transaction = con.BeginTransaction("SampleTransaction");
+                    cmd.Transaction = transaction;
+
+                    //parametros
+                    if (pro.cantidad  > 0)
+                    {
+                        ban = 1;
+                        int actual = pro.cantidad + cantidad;
+
+                        SqlParameter p1 = new SqlParameter("@cantidad", actual);
+                        SqlParameter p2 = new SqlParameter("@id", product_id);
+
+                        p1.SqlDbType = System.Data.SqlDbType.Int;
+                        p2.SqlDbType = System.Data.SqlDbType.Int;
+                        cmd.Parameters.Add(p1);
+                        cmd.Parameters.Add(p2);
+
+                        cmd.ExecuteNonQuery();
+                        transaction.Commit();
+                        MessageBox.Show("Actualizado con exito!");
+
+                    }
+                    else
+                    {
+                        ban = 0;
+                        transaction.Rollback();
+                        MessageBox.Show("La cantidad a recibir debe ser mayor a 0 (cero)");
+                    }
+                    con.Close();
+                }
+
+            }
+            return ban;
+        }
+
+        public static void Eliminar(EntradaProducto p)
+        {
+            listaEntrada.Remove(p);
+        }
+
+        public static List<EntradaProducto> Obtener()
+        {
+            return listaEntrada;
+        }
+
+
+
     }
 }
--- Presentacion/Formularios/frmEntrada.Designer.cs
+++ Presentacion/Formularios/frmEntrada.Designer.cs
@@ -36,12 +36,16 @@
             this.cmbProducto = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtCantidad = new System.Windows.Forms.TextBox();
-            this.dtgDetallePedido = new System.Windows.Forms.DataGridView();
+            this.dtgDetalleEntradaProducto = new System.Windows.Forms.DataGridView();
             this.lblFechaRemision = new System.Windows.Forms.Label();
             this.dtpFechaRemision = new System.Windows.Forms.DateTimePicker();
-            this.label1 = new System.Windows.Forms.Label();
-            this.cmbMotivo = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgDetallePedido)).BeginInit();
+            this.lblNroDoc = new System.Windows.Forms.Label();
+            this.txtNumeroDoc = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.lblReceptor = new System.Windows.Forms.Label();
+            this.txtReceptor = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDetalleEntradaProducto)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGuardar
@@ -53,6 +57,7 @@
             this.btnGuardar.TabIndex = 26;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // label4
             // 
@@ -71,6 +76,7 @@
             this.btnEliminar.TabIndex = 24;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // btnAgregar
             // 
@@ -80,11 +86,12 @@
             this.btnAgregar.TabIndex = 23;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // lblProducto
             // 
             this.lblProducto.AutoSize = true;
-            this.lblProducto.Location = new System.Drawing.Point(152, 86);
+            this.lblProducto.Location = new System.Drawing.Point(25, 46);
             this.lblProducto.Name = "lblProducto";
             this.lblProducto.Size = new System.Drawing.Size(50, 13);
             this.lblProducto.TabIndex = 22;
@@ -96,7 +103,7 @@
             this.cmbProducto.Items.AddRange(new object[] {
             "Vacuna",
             "Porcina"});
-            this.cmbProducto.Location = new System.Drawing.Point(208, 82);
+            this.cmbProducto.Location = new System.Drawing.Point(81, 42);
             this.cmbProducto.Name = "cmbProducto";
             this.cmbProducto.Size = new System.Drawing.Size(121, 21);
             this.cmbProducto.TabIndex = 17;
@@ -104,7 +111,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(38, 86);
+            this.label3.Location = new System.Drawing.Point(24, 86);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 21;
@@ -112,23 +119,23 @@
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(122, 82);
+            this.txtCantidad.Location = new System.Drawing.Point(108, 82);
             this.txtCantidad.Name = "txtCantidad";
             this.txtCantidad.Size = new System.Drawing.Size(24, 20);
             this.txtCantidad.TabIndex = 18;
             // 
-            // dtgDetallePedido
+            // dtgDetalleEntradaProducto
             // 
-            this.dtgDetallePedido.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgDetallePedido.Location = new System.Drawing.Point(27, 154);
-            this.dtgDetallePedido.Name = "dtgDetallePedido";
-            this.dtgDetallePedido.Size = new System.Drawing.Size(451, 198);
-            this.dtgDetallePedido.TabIndex = 20;
+            this.dtgDetalleEntradaProducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDetalleEntradaProducto.Location = new System.Drawing.Point(27, 154);
+            this.dtgDetalleEntradaProducto.Name = "dtgDetalleEntradaProducto";
+            this.dtgDetalleEntradaProducto.Size = new System.Drawing.Size(451, 198);
+            this.dtgDetalleEntradaProducto.TabIndex = 20;
             // 
             // lblFechaRemision
             // 
             this.lblFechaRemision.AutoSize = true;
-            this.lblFechaRemision.Location = new System.Drawing.Point(38, 22);
+            this.lblFechaRemision.Location = new System.Drawing.Point(24, 22);
             this.lblFechaRemision.Name = "lblFechaRemision";
             this.lblFechaRemision.Size = new System.Drawing.Size(83, 13);
             this.lblFechaRemision.TabIndex = 19;
@@ -137,33 +144,70 @@
             // dtpFechaRemision
             // 
             this.dtpFechaRemision.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpFechaRemision.Location = new System.Drawing.Point(127, 16);
+            this.dtpFechaRemision.Location = new System.Drawing.Point(113, 16);
             this.dtpFechaRemision.Name = "dtpFechaRemision";
             this.dtpFechaRemision.Size = new System.Drawing.Size(89, 20);
             this.dtpFechaRemision.TabIndex = 14;
             // 
-            // label1
+            // lblNroDoc
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 50);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
-            this.label1.TabIndex = 15;
-            this.label1.Text = "Motivo";
+            this.lblNroDoc.AutoSize = true;
+            this.lblNroDoc.Location = new System.Drawing.Point(285, 75);
+            this.lblNroDoc.Name = "lblNroDoc";
+            this.lblNroDoc.Size = new System.Drawing.Size(47, 13);
+            this.lblNroDoc.TabIndex = 51;
+            this.lblNroDoc.Text = "Nro Doc";
             // 
-            // cmbMotivo
+            // txtNumeroDoc
             // 
-            this.cmbMotivo.FormattingEnabled = true;
-            this.cmbMotivo.Location = new System.Drawing.Point(127, 42);
-            this.cmbMotivo.Name = "cmbMotivo";
-            this.cmbMotivo.Size = new System.Drawing.Size(203, 21);
-            this.cmbMotivo.TabIndex = 16;
+            this.txtNumeroDoc.Location = new System.Drawing.Point(354, 72);
+            this.txtNumeroDoc.Name = "txtNumeroDoc";
+            this.txtNumeroDoc.Size = new System.Drawing.Size(124, 20);
+            this.txtNumeroDoc.TabIndex = 50;
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(285, 42);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(52, 13);
+            this.lblDireccion.TabIndex = 49;
+            this.lblDireccion.Text = "Direccion";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(354, 39);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(124, 20);
+            this.txtDireccion.TabIndex = 48;
+            // 
+            // lblReceptor
+            // 
+            this.lblReceptor.AutoSize = true;
+            this.lblReceptor.Location = new System.Drawing.Point(285, 16);
+            this.lblReceptor.Name = "lblReceptor";
+            this.lblReceptor.Size = new System.Drawing.Size(51, 13);
+            this.lblReceptor.TabIndex = 47;
+            this.lblReceptor.Text = "Receptor";
+            // 
+            // txtReceptor
+            // 
+            this.txtReceptor.Location = new System.Drawing.Point(354, 13);
+            this.txtReceptor.Name = "txtReceptor";
+            this.txtReceptor.Size = new System.Drawing.Size(124, 20);
+            this.txtReceptor.TabIndex = 46;
             // 
             // frmEntrada
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(530, 460);
+            this.Controls.Add(this.lblNroDoc);
+            this.Controls.Add(this.txtNumeroDoc);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.lblReceptor);
+            this.Controls.Add(this.txtReceptor);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnEliminar);
@@ -172,15 +216,14 @@
             this.Controls.Add(this.cmbProducto);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtCantidad);
-            this.Controls.Add(this.dtgDetallePedido);
+            this.Controls.Add(this.dtgDetalleEntradaProducto);
             this.Controls.Add(this.lblFechaRemision);
             this.Controls.Add(this.dtpFechaRemision);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.cmbMotivo);
             this.Name = "frmEntrada";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Entrada de Productos";
-            ((System.ComponentModel.ISupportInitialize)(this.dtgDetallePedido)).EndInit();
+            this.Load += new System.EventHandler(this.frmEntradaProducto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDetalleEntradaProducto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,10 +239,14 @@
         private System.Windows.Forms.ComboBox cmbProducto;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtCantidad;
-        private System.Windows.Forms.DataGridView dtgDetallePedido;
+        private System.Windows.Forms.DataGridView dtgDetalleEntradaProducto;
         private System.Windows.Forms.Label lblFechaRemision;
         private System.Windows.Forms.DateTimePicker dtpFechaRemision;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox cmbMotivo;
+        private System.Windows.Forms.Label lblNroDoc;
+        private System.Windows.Forms.TextBox txtNumeroDoc;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label lblReceptor;
+        private System.Windows.Forms.TextBox txtReceptor;
     }
 }
