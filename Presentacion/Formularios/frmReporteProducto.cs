c026b4e5f1351243eec6e58bacafc039235e40be
sebastianpratt <sebastianpratt98@gmail.com>
2019-12-03 14:06:48 -0400

    Creacion del reporte de Productos

---
+++ Presentacion/Formularios/frmReporteProducto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace capaPresentacion.Formularios
+{
+    public partial class frmReporteProducto : Form
+    {
+        private ReporteProductos ReporteProductos1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+
+        public frmReporteProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.ReporteProductos1 = new capaPresentacion.ReporteProductos();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.ReporteProductos1;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(284, 261);
+            this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // frmReporteProducto
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "frmReporteProducto";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
