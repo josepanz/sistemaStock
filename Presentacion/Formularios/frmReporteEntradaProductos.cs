05875e40546e0c8385d5867e02fba765dcc31677
sebastianpratt <sebastianpratt98@gmail.com>
2019-12-11 15:54:00 -0400

    Modificacion del Reporte de Entrada

--- Presentacion/Formularios/frmReporteEntradaProductos.cs
+++ Presentacion/Formularios/frmReporteEntradaProductos.cs
@@ -12,7 +12,7 @@ namespace capaPresentacion.Formularios
 {
     public partial class frmReporteEntradaProductos : Form
     {
-        private ReporteEntadaProductos ReporteEntadaProductos1;
+        private ReporteEntradaProductos ReporteEntradaProductos1;
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
 
         public frmReporteEntradaProductos()
@@ -23,20 +23,21 @@ namespace capaPresentacion.Formularios
         private void InitializeComponent()
         {
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-            this.ReporteEntadaProductos1 = new capaPresentacion.Formularios.ReporteEntadaProductos();
+            this.ReporteEntradaProductos1 = new capaPresentacion.ReporteEntradaProductos();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
             // 
-            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.ActiveViewIndex = -1;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.ReportSource = this.ReporteEntadaProductos1;
+            this.crystalReportViewer1.ReportSource = this.ReporteEntradaProductos1;
             this.crystalReportViewer1.Size = new System.Drawing.Size(1234, 611);
             this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load);
             // 
             // frmReporteEntradaProductos
             // 
@@ -45,6 +46,16 @@ namespace capaPresentacion.Formularios
             this.Name = "frmReporteEntradaProductos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
+
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReporteEntadaProductos1_InitReport(object sender, EventArgs e)
+        {
 
         }
     }
